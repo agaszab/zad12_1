c7e5d025d973f6f2c83570cb1fca5fddd517f42f
agaszab <gatito@poczta.pl>
2018-02-18 17:03:58 +0100

    poprawione - zmiana rodzaju zbioru na listę

--- src/Program.java
+++ src/Program.java
@@ -5,17 +5,19 @@ public class Program {
 
         ArrayList<Integer> liczby = new ArrayList<>();
         Random rand = new Random();
+        int rozmiarListy=50;
 
         int suma=0;
 
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < rozmiarListy; i++) {
             liczby.add(rand.nextInt(1000)); // autoboxing
         }
+
 
        for (int liczba:liczby) {
            suma+=liczba;
         }
-        double srednia=suma/50;
+        double srednia=suma/rozmiarListy;
 
         Collections.sort(liczby);
         System.out.println("Liczby większe od średniej: ");
@@ -27,7 +29,7 @@ public class Program {
 
         System.out.println();
         System.out.println("Najmniejsza liczba ze zbioru: "+liczby.get(0));
-        System.out.println("Największa liczba ze zbioru: "+liczby.get(49));
+        System.out.println("Największa liczba ze zbioru: "+liczby.get(rozmiarListy-1));
         System.out.println("Średnia: "+srednia);
     }
 }
