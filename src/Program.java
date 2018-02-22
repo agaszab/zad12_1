395cb2060cae75a3e6393b76a6436583cd6b5183
agaszab <gatito@poczta.pl>
2018-02-22 11:30:11 +0100

    poprawione

--- src/Program.java
+++ src/Program.java
@@ -3,13 +3,12 @@ import java.util.*;
 public class Program {
     public static void main(String[] args) {
 
-        ArrayList<Integer> liczby = new ArrayList<>();
+        TreeSet <Integer> liczby= new TreeSet <>();
         Random rand = new Random();
-        int rozmiarListy=50;
 
         int suma=0;
 
-        for (int i = 0; i < rozmiarListy; i++) {
+        while (liczby.size()<50) {
             liczby.add(rand.nextInt(1000)); // autoboxing
         }
 
@@ -17,9 +16,8 @@ public class Program {
        for (int liczba:liczby) {
            suma+=liczba;
         }
-        double srednia=suma/rozmiarListy;
-
-        Collections.sort(liczby);
+        double srednia=suma/liczby.size();
+        
         System.out.println("Liczby większe od średniej: ");
         for (int liczba:liczby) {
             if (liczba>srednia)
@@ -28,8 +26,8 @@ public class Program {
 
 
         System.out.println();
-        System.out.println("Najmniejsza liczba ze zbioru: "+liczby.get(0));
-        System.out.println("Największa liczba ze zbioru: "+liczby.get(rozmiarListy-1));
+        System.out.println("Najmniejsza liczba ze zbioru: "+liczby.first());
+        System.out.println("Największa liczba ze zbioru: "+liczby.last());
         System.out.println("Średnia: "+srednia);
     }
 }
